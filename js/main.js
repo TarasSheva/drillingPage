2b7914a7c535b02b89e3208651c0f3ecca02b3dd
TarasSheva <tarasgsheva@gmail.com>
2022-08-16 15:30:59 +0300

    changed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-// const token = '5593072673:AAEaxtBKIGYlhW96AYpeeklSJ-tttKHFgWs'
 const token = '5495977901:AAFUOe2FHEplCg5YPMvDyu2BwsV5eUqEGhw'
-// const chatId = '-1001668145007'
 const chatId = '-1001656053766'
 const urlApi = `https://api.telegram.org/bot${token}/sendMessage`
 const modal = document.getElementById('modal')
@@ -36,3 +34,38 @@ form.addEventListener('submit', function (e) {
             modal.style.display = 'flex'
         })
 })
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
